955e23056446f0f3a065524809288512bad2679b
JesusEspitia <leopoldo.espitia@gmail.com>
2017-01-09 14:41:19 -0800

    Actualizacion 09/01 pt2.

--- Monotoring/Monotoring/Controllers/NotificationController.cs
+++ Monotoring/Monotoring/Controllers/NotificationController.cs
@@ -13,15 +13,36 @@ namespace Monotoring.Controllers
         // GET: Notification
         public ActionResult Index()
         {
+            int ordenId;
+            
             if (Session["userName"].ToString() != "")
             {
                 var qarea = from a in context.Area
                             where a.UsersId == (int)Session["userId"]
                             select a.AreaId;
                 int areaId = qarea.First();
-
+                if(areaId == 1)
+                {
+                    var qOrden = from w in context.WorkOrden
+                                where w.dateStart == null
+                                select w;
+                    return View(qOrden);
+                }
+                else
+                {
+                    var query = from a in context.Area_Orden
+                                join w in context.WorkOrden on a.WorkOrdenId equals w.WorkOrdenId
+                                where a.AreaId == (int)Session["userId"]
+                                select w;
+                    return View(query);
+                }
             }
-            return View();
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+           
+            
         }
     }
 }
