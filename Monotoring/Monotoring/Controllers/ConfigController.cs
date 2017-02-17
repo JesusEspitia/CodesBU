09fd83ab9efa15eeec2935632074de64d9847db9
JesusEspitia <leopoldo.espitia@gmail.com>
2017-02-16 16:11:12 -0800

    Actualizacion 16/02 pt2

---
+++ Monotoring/Monotoring/Controllers/ConfigController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monotoring.Context;
+
+
+namespace Monotoring.Controllers
+{
+    public class ConfigController : Controller
+    {
+        private TrackContext context = new TrackContext();
+        // GET: Config
+        public ActionResult Index()
+        {
+            if (Convert.ToString(Session["userType"]) == "3")
+            {
+                var area = context.Area.ToList();
+                ViewBag.Area = area;
+                var employee = context.Employee_type.ToList();
+                ViewBag.Employee = employee;
+                var family = context.FamilyProduct.ToList();
+                ViewBag.Family = family;
+                var code = context.DelayCode.ToList();
+                ViewBag.Code = code;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
