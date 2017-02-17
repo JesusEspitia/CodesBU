007e9fda12b821d9692695ca21fbeec3e55c488b
JesusEspitia <leopoldo.espitia@gmail.com>
2017-02-16 17:13:29 -0800

    Actualizacion 16/02 pt3

---
+++ Monotoring/Monotoring/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monotoring.Context;
+
+namespace Monotoring.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private TrackContext context = new TrackContext();
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            if (Convert.ToString(Session["userType"]) == "3" || Convert.ToString(Session["userType"]) == "2")
+            {
+                var orden = context.WorkOrden.Include("Catalog").Where(o => o.dateStart != null).ToList();
+                ViewBag.Orden = orden;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
