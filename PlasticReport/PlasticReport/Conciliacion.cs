0b893274bf9649508deccecf7b08710aa6719a75
JesusEspitia <leopoldo.espitia@gmail.com>
2017-03-01 17:52:40 -0800

    Conciliacion reportes nuevo

---
+++ PlasticReport/PlasticReport/Conciliacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlasticReport
+{
+    public partial class Conciliacion : Form
+    {
+        public Conciliacion()
+        {
+            InitializeComponent();
+        }
+
+        private void Conciliacion_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PlasticReport/PlasticReport/PlasticDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace PlasticReport
+{
+    public class PlasticDB
+    {
+        private MySqlConnection mycon;
+        private MySqlCommand cmd = new MySqlCommand();
+        public PlasticDB()
+        {
+            try
+            {
+                mycon = new MySqlConnection("SERVER=localhost;DATABASE=plasticreport;UID=root;PASSWORD=stringFoster9324;");
+                mycon.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al momento de conectar a la base de dato" + ex.Message, "Error al conectar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+}
