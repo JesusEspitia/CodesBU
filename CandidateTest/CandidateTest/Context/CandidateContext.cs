32f61d30b2e481fc824e83a90ec846f8f13fed80
JesusEspitia <leopoldo.espitia@gmail.com>
2017-04-02 23:41:14 -0700

    Cuetionario

---
+++ CandidateTest/CandidateTest/Context/CandidateContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CandidateTest.Models;
+using MySql.Data.Entity;
+
+namespace CandidateTest.Context
+{
+    //[DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class CandidateContext:DbContext
+    {
+    }
+}
---
+++ CandidateTest/CandidateTest/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CandidateTest.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        [DisplayName("Pregunta:")]
+        public string ContenQuestion { get; set; }
+        public int CandidateId { get; set; }
+
+    }
+}
